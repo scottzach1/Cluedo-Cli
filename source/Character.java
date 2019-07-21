962d1967dd1f9fb55fc4fe21fb16b14f7e8f629c
HonsonCooky <HonsonCooky@github.com>
2019-07-21 21:10:19 +1200

    Update Character.java

--- source/Character.java
+++ source/Character.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-public class Character implements Card {
+public class Character extends Card {
 	
 	public static enum characterAlias{
-		 Miss_Scarlett, 
-		 Colonel_Mustard, 
-		 Mrs_White, 
-		 Mr_Green,
-		 Mrs_Peacock,
-		 Professor_Plum;
+		 MIS_SCARLETT, 
+		 COLONEL_MUSTARD, 
+		 MRS_WHITE, 
+		 MR_GREEN,
+		 MRS_PEACOCK,
+		 PROFESSOR_PLUM;
 	}
 
 	// ------------------------
@@ -24,37 +24,62 @@ public class Character implements Card {
 	// CONSTRUCTOR
 	// ------------------------
 
-	public Character(User aUser, String aName) {		
+	public Character(String aName, User aUser) {	
+		super(aName);
+		
 		// Set user
 		if (aUser == null || aUser.getCharacter() != null) {
 			throw new RuntimeException("Unable to create Character due to invalid User");
 		}
 		user = aUser;
 		
-		// Set characterAlias
-		
-		
+		// Set character alias
+		switch(aName) {
+		case "MIS_SCARLETT":
+			charAlias = characterAlias.MIS_SCARLETT;
+			break;
+		case "COLONEL_MUSTARD":
+			charAlias = characterAlias.COLONEL_MUSTARD;
+			break;
+		case "MRS_WHITE":
+			charAlias = characterAlias.MRS_WHITE;
+			break;
+		case "MR_GREEN":
+			charAlias = characterAlias.MR_GREEN;
+			break;
+		case "MRS_PEACOCK":
+			charAlias = characterAlias.MRS_PEACOCK;
+			break;
+		case "PROFESSOR_PLUM":
+			charAlias = characterAlias.PROFESSOR_PLUM;
+			break;
+		default:
+			throw new RuntimeException("Character alias not found.");
+		}	
 	}
-
+	
+	
 	// ------------------------
 	// INTERFACE
 	// ------------------------
-
-	@Override
-	public String getName(String aName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void getName() {
-		// TODO Auto-generated method stub
-		
-	}
 	
-	@Override
-	public String toString() {
-		return "Yet to implement";
+
+	public User getUser() {
+		return user;
 	}
 
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public characterAlias getCharAlias() {
+		return charAlias;
+	}
+
+
+	public void setCharAlias(characterAlias charAlias) {
+		this.charAlias = charAlias;
+	}
 }
