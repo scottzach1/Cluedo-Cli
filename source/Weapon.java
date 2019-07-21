68e4cee7c2c7ff9db384dbe0069b26952c60c15c
HonsonCooky <HonsonCooky@github.com>
2019-07-21 21:10:27 +1200

    Update Weapon.java

--- source/Weapon.java
+++ source/Weapon.java
@@ -1,84 +1,33 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.29.1.4584.3d417815a modeling language!*/
+public class Weapon extends Card {
 
+	// ------------------------
+	// MEMBER VARIABLES
+	// ------------------------
 
-import java.util.*;
+	// Weapon Attributes
+	private Room location;
 
-// line 66 "model.ump"
-// line 118 "model.ump"
-public class Weapon extends Card
-{
+	// ------------------------
+	// CONSTRUCTOR
+	// ------------------------
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+	public Weapon(String aName) {
+		super(aName);
+	}
 
-  //Weapon Attributes
-  private Room location;
+	// ------------------------
+	// INTERFACE
+	// ------------------------
 
-  //Weapon Associations
-  private Room room;
+	public Room getLocation() {
+		return location;
+	}
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
+	public void setLocation(Room location) {
+		this.location = location;
+	}	
 
-  public Weapon(String aName, Room aLocation, Room aRoom)
-  {
-    super(aName);
-    location = aLocation;
-    if (aRoom == null || aRoom.getWeapon() != null)
-    {
-      throw new RuntimeException("Unable to create Weapon due to aRoom. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    room = aRoom;
-  }
-
-  public Weapon(String aName, Room aLocation, String aNameForRoom, List aCellsForRoom, Cell aCellForRoom)
-  {
-    super(aName);
-    location = aLocation;
-    room = new Room(aNameForRoom, aCellsForRoom, aCellForRoom, this);
-  }
-
-  //------------------------
-  // INTERFACE
-  //------------------------
-
-  public boolean setLocation(Room aLocation)
-  {
-    boolean wasSet = false;
-    location = aLocation;
-    wasSet = true;
-    return wasSet;
-  }
-
-  public Room getLocation()
-  {
-    return location;
-  }
-  /* Code from template association_GetOne */
-  public Room getRoom()
-  {
-    return room;
-  }
-
-  public void delete()
-  {
-    Room existingRoom = room;
-    room = null;
-    if (existingRoom != null)
-    {
-      existingRoom.delete();
-    }
-    super.delete();
-  }
-
-
-  public String toString()
-  {
-    return super.toString() + "["+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "location" + "=" + (getLocation() != null ? !getLocation().equals(this)  ? getLocation().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
-            "  " + "room = "+(getRoom()!=null?Integer.toHexString(System.identityHashCode(getRoom())):"null");
-  }
+	public String toString() {
+		return "";
+	}
 }
